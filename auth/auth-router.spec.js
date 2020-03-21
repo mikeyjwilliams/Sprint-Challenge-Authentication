fd6b5fa65911505fdad04bb3969d240d517bd2dd
Michael Williams <manicdesigner@gmail.com>
2020-03-21 13:41:48 -0400

    working on tests for TDD

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -3,27 +3,39 @@
 const supertest = require('supertest');
 const server = require('../api/server');
 
+let user = 'miguel';
+
 describe('login route', () => {
   describe('login', () => {
     test('login pass /api/auth/register', async () => {
       const res = await supertest(server)
         .post('/api/auth/register')
-        .send({ username: 'mikey', password: '123' });
+        .send({ username: user, password: '123' });
 
       expect(res.statusCode).toBe(201);
       expect(res.type).toBe('application/json');
-      expect(res.body.username).toBe('mikey');
+      expect(res.body.username).toBe(user);
+    });
+
+    test('login fail /api/auth/register', async () => {
+      const res = await supertest(server)
+        .post('/api/auth/register')
+        .send({ username: 'mikey', password: '123' });
+
+      expect(res.statusCode).toBe(409);
+      expect(res.type).toBe('application/json');
+      expect(res.body.message).toMatch(/username/i);
     });
   });
 });
 
 describe('register route', () => {
-  describe('register pass', () => {
+  describe('register fail', () => {
     test('register /api/auth/login', async () => {
       const res = await supertest(server)
         .post('/api/auth/login')
         .send({ username: 'mikey', password: '123' });
-      expect(res.statusCode).toBe(200);
+      expect(res.statusCode).toBe(401);
       expect(res.type).toBe('application/json');
     });
   });
