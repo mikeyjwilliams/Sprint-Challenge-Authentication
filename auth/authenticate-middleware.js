0996ea45ceaa0d9463cec8dea30f36ca2c9ad6b1
Michael Williams <manicdesigner@gmail.com>
2020-03-21 14:40:08 -0400

    got authenticate--middleware to work

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,28 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+/**
+ * /*
+ *   complete the middleware code to check if the user is logged in
+ *   before granting access to the next middleware/route handler
+ *
+ * @format
+ */
+const secret = require('../Secret/secret');
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { token } = req.cookies;
+  console.log(token, 'token');
+  if (!token) {
+    console.log('1st');
+    return res.status(401).json({ you: 'shall not pass!' });
+  }
+  jwt.verify(token, secret.jwtSecret, (err, decoded) => {
+    if (err) {
+      console.log('2nd');
+      return res.status(401).json({ you: 'shall not pass!' });
+    } else {
+      req.token = decoded;
+      console.log('D ', decoded);
+      next();
+    }
+  });
 };
