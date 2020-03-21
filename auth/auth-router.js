2ec6cffef8555c1e296ace133ce80cbba4e09fda
Michael Williams <manicdesigner@gmail.com>
2020-03-21 12:31:02 -0400

    sign in done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,27 @@
+/** @format */
+
 const router = require('express').Router();
+const authModel = require('./auth-model');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await authModel.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({ message: 'username is taken' });
+    }
+    const userAdd = {
+      username: username,
+      password: password,
+    };
+    res.status(201).json(userAdd);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'server error logging in' });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+router.post('/login', async (req, res) => {});
 
 module.exports = router;
