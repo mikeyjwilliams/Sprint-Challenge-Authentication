68f9c32f7c13ee3abd03e98427f110f031ffaa7c
Michael Williams <manicdesigner@gmail.com>
2020-03-21 13:04:44 -0400

    add spec tests for login and register

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -4,13 +4,26 @@ const supertest = require('supertest');
 const server = require('../api/server');
 
 describe('login route', () => {
-  describe('login fails', () => {
-    it('login /api/auth/register', async () => {
+  describe('login', () => {
+    test('login pass /api/auth/register', async () => {
       const res = await supertest(server)
         .post('/api/auth/register')
         .send({ username: 'mikey', password: '123' });
 
-      expect(res.statusCode).toBe(409);
+      expect(res.statusCode).toBe(201);
+      expect(res.type).toBe('application/json');
+      expect(res.body.username).toBe('mikey');
+    });
+  });
+});
+
+describe('register route', () => {
+  describe('register pass', () => {
+    test('register /api/auth/login', async () => {
+      const res = await supertest(server)
+        .post('/api/auth/login')
+        .send({ username: 'mikey', password: '123' });
+      expect(res.statusCode).toBe(200);
       expect(res.type).toBe('application/json');
     });
   });
