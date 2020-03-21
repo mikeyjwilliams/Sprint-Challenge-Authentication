8b9ead2bd956fb86076c76b0d1fb71d7c27db56a
Michael Williams <manicdesigner@gmail.com>
2020-03-21 13:42:24 -0400

    add requires to auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,6 +2,8 @@
 
 const router = require('express').Router();
 const authModel = require('./auth-model');
+const bcrypt = require('bcryptjs');
+const secret = require('../Secret/secret');
 
 router.post('/register', async (req, res) => {
   try {
@@ -15,13 +17,45 @@ router.post('/register', async (req, res) => {
       username: username,
       password: password,
     };
-    res.status(201).json(userAdd);
+    const userLogin = await authModel.add(userAdd);
+    res.status(201).json(userLogin);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'server error logging in' });
   }
 });
 
-router.post('/login', async (req, res) => {});
+router.post('/login', async (req, res) => {
+  const authErr = { message: 'Invalid Credentials' };
+  const { username, password } = req.body;
+  try {
+    const user = await authModel.findBy({ username }).first();
+
+    if (!user) {
+      console.log('not pass user');
+      res.status(401).json(authErr);
+    }
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+
+    if (!passwordCheck) {
+      console.log('not pass password check');
+      return res.status(401).json(authErr);
+    }
+
+    const payload = {
+      userId: user.id,
+    };
+    const token = jwt.sign(payload, secret.jwtSecret);
+    res.cookie = token;
+    res.json({
+      message: `Greetings ${user.username}`,
+      token: token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'server error logging in' });
+  }
+});
 
 module.exports = router;
